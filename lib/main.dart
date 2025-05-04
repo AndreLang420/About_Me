5ddd4a35cea77f592b2a2a965db72bc1c3763085
unknown <cob.yish02@gmail.com>
2025-05-04 21:12:29 +0800

    'shermanMODEl'

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoadingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+
+    Future.delayed(Duration(seconds: 6), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomeScreen()), 
+      );
+    });
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/about_me_loading_screen.jpg'),
+            CircularProgressIndicator(),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget { 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Home Screen!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
